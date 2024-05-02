bd48e8a9a61fe4f40af53b119a64c83c70c4d8a2
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-05-02 11:00:54 +0530

    Day13

---
+++ Day13/MVCServletJSP/src/main/java/com/tka/servlet/DashboardServlet.java
@@ -0,0 +1,48 @@
+package com.tka.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class DashboardServlet
+ */
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashboardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("U r in Dashboard Servlet....");
+		HttpSession session= request.getSession();
+		String username=(String) session.getAttribute("username");
+		PrintWriter pw= response.getWriter();
+		pw.write("Username... "+username);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	//	doGet(request, response);
+	}
+
+}
